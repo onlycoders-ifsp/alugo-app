5f57445284d9dab5cc81ba0b9449c7d77e2967fd
Dilan Lima <the.dilanlima@gmail.com>
2020-10-03 06:20:10 -0300

    modulo de clientes desenhado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClienteLayoutComponent } from './cliente-layout/cliente-layout.component';
+import { DetalheProdutoComponent } from './detalhe-produto/detalhe-produto.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { TodosProdutosComponent } from './todos-produtos/todos-produtos.component';
@@ -9,7 +10,8 @@ import { TodosProdutosComponent } from './todos-produtos/todos-produtos.componen
 const routes: Routes = [
   { path: '', component: ClienteLayoutComponent, children: [
     {path: '', component: HomeComponent},
-    {path: 'list-all', component: TodosProdutosComponent}
+    {path: 'list-all', component: TodosProdutosComponent},
+    {path: 'detalhe-produto', component: DetalheProdutoComponent}
   ] },
   { path: 'login', component: LoginComponent }
 ];
--- src/app/cliente/cliente-produto/cliente-produto.component.ts
+++ src/app/cliente/cliente-produto/cliente-produto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cliente-produto',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteProdutoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  verProdutoDeslogado(){
+    this.router.navigate(['/detalhe-produto']);
+  }
+
+  verListaProdutos(){
+    this.router.navigate(['/list-all']);
+  }
+  VerPerfil(){
+    this.router.navigate(['/cliente/perfil']);
+  }
+  verMeuProduto(){
+    this.router.navigate(['/cliente/produto']);
+  }
+  voltaHome(){
+    this.router.navigate(['']);
   }
 
 }
